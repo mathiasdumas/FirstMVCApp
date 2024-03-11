b7c636087621ffbd6a62eef10dd0a7ae7e05d2c7
mathias <mathiasdumaspro@gmail.com>
2024-03-11 21:58:03 +0100

    first commit

---
+++ Controllers/ClassroomController.cs
@@ -0,0 +1,22 @@
+﻿using FirstAspDotnetApp.ViewModels.Classroom;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstAspDotnetApp.Controllers
+{
+    public class ClassroomController : Controller
+    {
+        public IActionResult Index()
+        {
+            var model = new IndexViewModel();
+            return View(model);
+        }
+
+        public IActionResult Students(int Id)
+        {
+            var model = new IndexViewModel();
+            ViewData["id"] = Id;
+            return View(model);
+        }
+
+    }
+}
---
+++ ViewModels/Home/IndexViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace FirstAspDotnetApp.ViewModels.Home
+{
+    public class IndexViewModel
+    {
+        public string? CompanyName { get; set; }
+        public IndexViewModel(string companyName)
+        {
+            CompanyName = companyName;
+        }
+    }
+}
